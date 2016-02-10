11ca8295755ad5ca9b842a253d93c096649ee260
griffinjared <jgriffin@iastate.edu>
2016-02-10 10:45:25 -0600

    Created "SeedGenerator" class

---
+++ app/src/main/java/com/gameapp/gameapp/SeedGenerator.java
@@ -0,0 +1,17 @@
+package com.gameapp.gameapp;
+
+import java.util.Random;
+
+/**
+ * Created by griff on 2/10/2016.
+ */
+public class SeedGenerator {
+    private Random seeder = new Random();
+    private final int SEED = seeder.nextInt(10);
+
+    public SeedGenerator() {}
+
+    public int getSeed() {
+        return SEED;
+    }
+}
