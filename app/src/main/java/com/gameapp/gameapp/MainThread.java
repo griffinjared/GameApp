f9312c52e6f96ebc713d5b80a0a17861904ff140
Andrew <abk7997@iastate.edu>
2016-02-21 18:25:57 -0600

    Big change to the Main Thread Functions the same as before but is capped at 30 FPS

--- app/src/main/java/com/gameapp/gameapp/MainThread.java
+++ app/src/main/java/com/gameapp/gameapp/MainThread.java
@@ -1,5 +1,6 @@
 package com.gameapp.gameapp;
 
+import android.graphics.Canvas;
 import android.util.Log;
 import android.view.SurfaceHolder;
 
@@ -7,28 +8,84 @@ public class MainThread extends Thread {
 
     private static final String TAG = MainThread.class.getSimpleName();
 
+    //Main components
     private SurfaceHolder surfaceHolder;
-    private gameClass gameClass;
+    private Game game; //Primary class
+    private gameClass start;
+    public static Canvas canvas;
+
+    //FPS
+    private int FPS = 30;
+    private double averageFPS;
+
     private boolean running;
 
     public void setRunning(boolean running) {
         this.running = running;
     }
 
-    public MainThread(SurfaceHolder surfaceHolder, gameClass gameClass) {
+    public MainThread(SurfaceHolder surfaceHolder, gameClass start, Game game) {
         super();
         this.surfaceHolder = surfaceHolder;
-        this.gameClass = gameClass;
+        this.start = start;
+        this.game = game;
     }
 
     @Override
     public void run() {
+        //Variable for keeping track of time and FPS
+        long startTime;
+        long timeMillis;
+        long waitTime;
+        long totalTime = 0;
+        int frameCount = 0;
+        long targetTime = 1000/FPS;
+
+        //Others
         long tickCount = 0L;
         Log.d(TAG, "Starting game loop");
         while (running) {
             tickCount++;
-            //update game state
-            //render state to screen
+
+            startTime = System.nanoTime();
+            canvas = null;
+
+            //lock canvas
+            try {
+                canvas = this.surfaceHolder.lockCanvas();
+                synchronized (surfaceHolder) {
+                    //this.game.update();
+                    //this.game.draw(canvas);
+                }
+            } catch(Exception e) {}
+
+            finally {
+                if (canvas != null) {
+                    try {
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                    }
+                    catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            timeMillis = (System.nanoTime() - startTime) / 1000000;
+            waitTime = targetTime - timeMillis;
+
+            try {
+                this.sleep(waitTime);
+            } catch(Exception e) {}
+
+            totalTime += System.nanoTime();
+            frameCount++;
+
+            if (frameCount == FPS) {
+                averageFPS = 1000 / (totalTime/frameCount)/1000000;
+                frameCount = 0;
+                totalTime = 0;
+                System.out.println(averageFPS);
+            }
         }
         Log.d(TAG, "Game loop executed " + tickCount + " times");
     }
