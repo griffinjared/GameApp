a206838d65ce300abff048bbae0133ee7e5f724a
Andrew <abk7997@iastate.edu>
2016-02-26 14:25:02 -0600

    Rooms will now automatically have the correct number of doorways based on their position on the map.

--- app/src/main/java/levels/Level_1_Forest.java
+++ app/src/main/java/levels/Level_1_Forest.java
@@ -17,31 +17,65 @@ public class Level_1_Forest extends Level {
         changeTilesSprites(spriteSheet);
 
         levelLayout = new char[][] {
-                {'E', 'R', 'E'},
-                {'X', 'S', 'D'},
-                {'E', 'E', 'E'}
+                {' ', ' ', ' ', ' ', ' '},
+                {' ', 'E', 'R', 'E', ' '},
+                {' ', 'X', 'S', 'D', ' '},
+                {' ', 'E', 'E', 'E', ' '},
+                {' ', ' ', ' ', ' ', ' '}
         };
 
         for (int y = 0; y < levelLayout.length; y++) {
             for (int x = 0; x < levelLayout[y].length; x++) {
                 switch(levelLayout[y][x]) {
-                    case 'E': rooms.add(new Empty_Room(context, 0)); break;
-                    case 'S': rooms.add(new Spawn_Room(context, 0)); break;
-                    case 'X': rooms.add(new Exit_Room(context, 0)); break;
-                    case 'R': rooms.add(new River_Room(context, 0)); break;
-                    case 'D': rooms.add(new Deforested_Room(context, 0)); break;
-                    default: break;
+                    case 'E': rooms.add(new Empty_Room(context, configureDoors(x, y))); break;
+                    case 'S': rooms.add(new Spawn_Room(context, configureDoors(x, y))); break;
+                    case 'X': rooms.add(new Exit_Room(context, configureDoors(x, y))); break;
+                    case 'R': rooms.add(new River_Room(context, configureDoors(x, y))); break;
+                    case 'D': rooms.add(new Deforested_Room(context, configureDoors(x, y))); break;
+                    default: rooms.add(new Room(context)); break;
                 }
             }
         }
 
-        roomY = 1;
-        roomX = 1;
+        roomY = 2;
+        roomX = 2;
         currentRoom = rooms.get(roomX + (roomY*levelLayout[roomX].length));
     }
 
     public Room changeRoom(int roomX, int roomY) {
         return rooms.get(roomX + (roomY*levelLayout[roomX].length));
+    }
+
+    public int configureDoors(int roomX, int roomY) {
+        boolean north = false, south = false, east = false, west = false;
+
+        if (levelLayout[roomY-1][roomX] != ' ') north = true; //north
+        if (levelLayout[roomY+1][roomX] != ' ') south = true; //south
+        if (levelLayout[roomY][roomX-1] != ' ') west = true; //west
+        if (levelLayout[roomY][roomX+1] != ' ') east = true; //east
+
+        //Assignment
+        if (north && south && east && west) return 0;
+
+        else if (north && east && south) return 11;
+        else if (east && south && west) return 12;
+        else if (south && west && north) return 13;
+        else if (west && north && east) return 14;
+
+        else if (north && south) return 1;
+        else if (east && west) return 2;
+
+        else if (south && east) return 3;
+        else if (south && west) return 4;
+        else if (north && east) return 5;
+        else if (north && west) return 6;
+
+        else if (north) return 7;
+        else if (south) return 8;
+        else if (east) return 9;
+        else if (west) return 10;
+
+        return 0;
     }
 
 }
